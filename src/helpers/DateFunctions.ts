359e3c7483f6f4d7da374db347f46f7d9f2fefc6
Vyacheslav <sheptikhinv@gmail.com>
2025-07-13 18:31:21 +0500

    add opportunity to see how much time till shift ends

--- src/helpers/DateFunctions.ts
+++ src/helpers/DateFunctions.ts
@@ -19,3 +19,42 @@ export const isToday = (date: Date) => {
         date.getDate() === today.getDate()
     );
 }
+
+export const timeTillDate = (date: Date): string => {
+    const now = new Date();
+    let diff = date.getTime() - now.getTime();
+
+    if (diff <= 0) return "00:00:00";
+
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    diff %= 1000 * 60 * 60;
+
+    const minutes = Math.floor(diff / (1000 * 60));
+    diff %= 1000 * 60;
+
+    const seconds = Math.floor(diff / 1000);
+
+    const pad = (num: number) => String(num).padStart(2, '0');
+
+    return `-${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+};
+
+export const getClosestTime = (hour: number, minute: number): Date => {
+    const now = new Date();
+    const target = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate(),
+        hour,
+        minute,
+        0,
+        0
+    );
+
+    if (target <= now) {
+        // Добавляем 1 день, если время уже прошло
+        target.setDate(target.getDate() + 1);
+    }
+
+    return target;
+};
